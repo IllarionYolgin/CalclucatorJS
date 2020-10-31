6f384ffacad9911bf5f184627efcfdaace5b8329
TheSCV <illarionyolgin@gmail.com>
2020-10-31 16:34:35 +0300

    Clearing up js from unused staff and comments

--- js/main.js
+++ js/main.js
@@ -1,28 +1,20 @@
-// Start writing JavaScript here!
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 const display = calculator.querySelector('.calculator__display')
-const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
 
-// let displayValue = display.textContent
 let displayValue = ''
 
-
 keys.addEventListener('click', event => {
-  console.log(event.target)
   if (!event.target.closest('button')) return
 
   const key = event.target
-  const keyValue = key.textContent
-  const { type } = key.dataset
-  const { previousKeyType } = calculator.dataset
 
   if (type === 'number' || type === 'operator') {
     displayValue += key.textContent;
   }
 
   if (type === 'equal') {
-    console.log(displayValue.replace('÷', '/').replace('×', '*'))
+    if(displayValue != '')
     displayValue = eval(displayValue.replace('÷', '/').replace('×', '*'))
     }
 
@@ -31,6 +23,4 @@ keys.addEventListener('click', event => {
   }
 
   display.value = displayValue
-
-  calculator.dataset.previousKeyType = type
 })
